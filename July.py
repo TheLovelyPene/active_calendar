7e42c77bd364f56926d4b03fed7e6bdb5636eee7
TheLovelyPene <penelope.trent@pursuit.org>
2025-06-24 19:40:04 -0400

    Implement initial project structure and setup

---
+++ July.py
@@ -0,0 +1,406 @@
+# This Python list contains dictionaries, where each dictionary represents a single free event in NYC.
+# This structure allows for easy organization and retrieval of event details.
+
+nyc_free_events_database = [
+    # --- Manhattan Events ---
+    {
+        "name": "Hudson River Park's Jazz at Pier 84",
+        "date": "2025-07-02",
+        "time": "7:00 PM – 8:30 PM",
+        "address": "Hudson River Park's Pier 84, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "NY Classical presents: All's Well That Ends Well",
+        "date": "2025-07-02",
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Castle Clinton (in The Battery), Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "NY Classical presents: All's Well That Ends Well",
+        "date": "2025-07-06",
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Castle Clinton (in The Battery), Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "New York Guitar Festival: Pedro Cortes Flamenco, Big Lazy, Marel Hidalgo",
+        "date": "2025-07-03",
+        "time": "7:00 PM",
+        "address": "Bryant Park Stage, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://bryantpark.org/activities/picnic-performances"
+    },
+    {
+        "name": "New York Guitar Festival: Louis Cato, Jackie Venson, Jontavious Willis",
+        "date": "2025-07-04",
+        "time": "7:00 PM",
+        "address": "Bryant Park Stage, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://bryantpark.org/activities/picnic-performances"
+    },
+    {
+        "name": "Macy's 4th of July Fireworks",
+        "date": "2025-07-04",
+        "time": "8:00 PM",
+        "address": "East River (viewable from Manhattan, Brooklyn, and Queens waterfronts)", # Note: Broad viewable area
+        "borough": "Manhattan (Primary launch area)",
+        "link": "https://www.timeout.com/newyork/events-calendar/july-events-calendar"
+    },
+    {
+        "name": "Taikoza Drumming Performance",
+        "date": "2025-07-08",
+        "time": "3:00 PM – 4:00 PM",
+        "address": "Jackie Robinson Park - Bandshell, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "Reading Rhythms at Hudson Yards",
+        "date": "2025-07-08",
+        "time": "TBD (likely daytime/evening)",
+        "address": "Hudson Yards, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycforfree.co/events"
+    },
+    {
+        "name": "Carnegie Hall Citywide: Toomai String Quintet",
+        "date": "2025-07-09",
+        "time": "TBD (likely evening)",
+        "address": "Madison Square Park, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.carnegiehall.org/About/Press/Press-Releases/2025/04/15/Carnegie-Hall-Citywide-Announces-20252026-Season-with-Free-Performances-Across-NYC"
+    },
+    {
+        "name": "River & Blues Concert Series (Maggie Rose)",
+        "date": "2025-07-10",
+        "time": "TBD (likely evening)",
+        "address": "Rockefeller Park, Battery Park City, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://secretnyc.co/free-summer-concerts-2025-full-list/"
+    },
+    {
+        "name": "Carnegie Hall Citywide: The Knights with Julien Labro",
+        "date": "2025-07-11",
+        "time": "7:00 PM",
+        "address": "Bryant Park Stage, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://bryantpark.org/activities/picnic-performances"
+    },
+    {
+        "name": "Carnegie Hall Citywide: Ziggy and Miles",
+        "date": "2025-07-16",
+        "time": "6:00 PM",
+        "address": "Madison Square Park, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.carnegiehall.org/About/Press/Press-Releases/2025/04/15/Carnegie-Hall-Citywide-Announces-20252026-Season-with-Free-Performances-Across-NYC"
+    },
+    {
+        "name": "Hudson River Park's Jazz at Pier 84",
+        "date": "2025-07-16",
+        "time": "7:00 PM – 8:30 PM",
+        "address": "Hudson River Park's Pier 84, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "River & Blues Concert Series (Amythyst Kiah)",
+        "date": "2025-07-17",
+        "time": "TBD (likely evening)",
+        "address": "Rockefeller Park, Battery Park City, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://secretnyc.co/free-summer-concerts-2025-full-list/"
+    },
+    {
+        "name": "Carnegie Hall Citywide: La Excelencia",
+        "date": "2025-07-18",
+        "time": "7:00 PM",
+        "address": "Bryant Park Stage, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://bryantpark.org/activities/picnic-performances"
+    },
+    {
+        "name": "Carnegie Hall Citywide: Catalyst Quartet",
+        "date": "2025-07-23",
+        "time": "TBD (likely evening)",
+        "address": "Madison Square Park, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.carnegiehall.org/About/Press/Press-Releases/2025/04/15/Carnegie-Hall-Citywide-Announces-20252026-Season-with-Free-Performances-Across-NYC"
+    },
+    {
+        "name": "River & Blues Concert Series (Afro Latin Jazz Orchestra)",
+        "date": "2025-07-24",
+        "time": "TBD (likely evening)",
+        "address": "Rockefeller Park, Battery Park City, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://secretnyc.co/free-summer-concerts-2025-full-list/"
+    },
+    {
+        "name": "Carnegie Hall Citywide: Cécile McLorin Salvant",
+        "date": "2025-07-25",
+        "time": "7:00 PM",
+        "address": "Bryant Park Stage, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://bryantpark.org/activities/picnic-performances"
+    },
+    {
+        "name": "Hudson River Park's Jazz at Pier 84",
+        "date": "2025-07-30",
+        "time": "7:00 PM – 8:30 PM",
+        "address": "Hudson River Park's Pier 84, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "River & Blues Concert Series (Lady Blackbird)",
+        "date": "2025-07-31",
+        "time": "TBD (likely evening)",
+        "address": "Rockefeller Park, Battery Park City, Manhattan",
+        "borough": "Manhattan",
+        "link": "https://secretnyc.co/free-summer-concerts-2025-full-list/"
+    },
+
+    # --- Brooklyn Events ---
+    {
+        "name": "Nathan's Hot Dog Eating Contest",
+        "date": "2025-07-04",
+        "time": "TBD (morning/afternoon)",
+        "address": "Nathan's Famous, 1310 Surf Ave, Coney Island, Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://www.iloveny.com/blog/post/things-to-do-this-july-in-new-york-state/"
+    },
+    {
+        "name": "Summer Market at Empire Stores in Dumbo",
+        "date": "2025-07-12 to 2025-07-13",
+        "time": "TBD (likely daytime)",
+        "address": "Empire Stores, Dumbo, Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://www.timeout.com/newyork/events-calendar/july-events-calendar"
+    },
+    {
+        "name": "BRIC Celebrate Brooklyn! (Still Woozy – Loveseat Tour)",
+        "date": "2025-07-12",
+        "time": "TBD (likely evening)",
+        "address": "Lena Horne Bandshell (Prospect Park), Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://bricartsmedia.org/celebrate-brooklyn/"
+    },
+    {
+        "name": "Park Pitch In: Lake Appreciation Month – Boathouse",
+        "date": "2025-07-13",
+        "time": "10:30 AM – 12:00 PM",
+        "address": "Prospect Park Boathouse, 101 East Dr, Prospect Park, Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://www.reddit.com/r/nyc/comments/1li14mo/things_to_do_in_nyc_july_2025/"
+    },
+    {
+        "name": "BRIC Celebrate Brooklyn! (Dinosaur Jr. & Snail Mail with Easy Action)",
+        "date": "2025-07-17",
+        "time": "TBD (likely evening)",
+        "address": "Lena Horne Bandshell (Prospect Park), Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://bricartsmedia.org/celebrate-brooklyn/"
+    },
+    {
+        "name": "BRIC Celebrate Brooklyn! (Men I Trust and strongboi)",
+        "date": "2025-07-18",
+        "time": "TBD (likely evening)",
+        "address": "Lena Horne Bandshell (Prospect Park), Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://bricartsmedia.org/celebrate-brooklyn/"
+    },
+    {
+        "name": "BRIC Celebrate Brooklyn! at Brower Park",
+        "date": "2025-07-19",
+        "time": "TBD (likely evening)",
+        "address": "Brower Park, Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://bricartsmedia.org/celebrate-brooklyn/"
+    },
+    {
+        "name": "Rise Up NYC Concert Series (Brooklyn Dates)",
+        "date": "2025-07-19 to 2025-07-21", # Series runs over these dates
+        "time": "TBD (check link closer to date)",
+        "address": "Venue details coming soon, Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://riseupnycconcerts.com/rise-up-nyc-2025-official-dates-announced-for-the-citys-biggest-free-concert-series/"
+    },
+    {
+        "name": "Governors Island Market",
+        "date": "2025-07-19 to 2025-07-20",
+        "time": "TBD (likely daytime)",
+        "address": "Governors Island (accessible via ferry from Manhattan or Brooklyn)",
+        "borough": "Brooklyn (accessible, shared)",
+        "link": "https://www.timeout.com/newyork/events-calendar/july-events-calendar"
+    },
+    {
+        "name": "Summer Market in Cobble Hill",
+        "date": "2025-07-26 to 2025-07-27",
+        "time": "TBD (likely daytime)",
+        "address": "Cobble Hill, Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://www.timeout.com/newyork/events-calendar/july-events-calendar"
+    },
+    {
+        "name": "BRIC Celebrate Brooklyn! (A Tribute to Quincy Jones: The Wiz)",
+        "date": "2025-07-27",
+        "time": "TBD (likely evening)",
+        "address": "Lena Horne Bandshell (Prospect Park), Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://bricartsmedia.org/celebrate-brooklyn/"
+    },
+    {
+        "name": "Bats! Night Walk",
+        "date": "2025-07-31",
+        "time": "7:30 PM – 9:00 PM",
+        "address": "Seba Playground, Seba Ave & Gerritsen Ave (Marine Park), Brooklyn",
+        "borough": "Brooklyn",
+        "link": "https://www.reddit.com/r/nyc/comments/1li14mo/things_to_do_in_nyc_july_2025/"
+    },
+
+    # --- Queens Events ---
+    {
+        "name": "2025 Waterfront Summer Concert Series (Astoria Park)",
+        "date": "2025-07-03", # First Thursday
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Great Lawn at Astoria Park, Queens",
+        "borough": "Queens",
+        "link": "https://www.nycgovparks.org/events/2025/07/17/2025-waterfront-summer-concert-series"
+    },
+    {
+        "name": "2025 Waterfront Summer Concert Series (Astoria Park)",
+        "date": "2025-07-10",
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Great Lawn at Astoria Park, Queens",
+        "borough": "Queens",
+        "link": "https://www.nycgovparks.org/events/2025/07/17/2025-waterfront-summer-concert-series"
+    },
+    {
+        "name": "Summer Concert Series - Forest Park Trust (Draw the Line - Aerosmith Tribute)",
+        "date": "2025-07-10",
+        "time": "7:30 PM",
+        "address": "George Seuffert, Sr. Bandshell (in Forest Park), Queens",
+        "borough": "Queens",
+        "link": "https://forestparktrust.org/events/2024-summer-concert-series-4acsh"
+    },
+    {
+        "name": "The Queens Jazz Trail Free Concert Series: Bryan Carrott",
+        "date": "2025-07-10",
+        "time": "7:00 PM – 8:00 PM",
+        "address": "Baisley Pond Park, Jamaica, Queens",
+        "borough": "Queens",
+        "link": "https://www.nycgovparks.org/events/free_summer_concerts"
+    },
+    {
+        "name": "2025 Waterfront Summer Concert Series (Astoria Park)",
+        "date": "2025-07-17",
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Great Lawn at Astoria Park, Queens",
+        "borough": "Queens",
+        "link": "https://www.nycgovparks.org/events/2025/07/17/2025-waterfront-summer-concert-series"
+    },
+    {
+        "name": "Summer Concert Series - Forest Park Trust (Zac N' Fried - Zac Brown Tribute)",
+        "date": "2025-07-17",
+        "time": "7:30 PM",
+        "address": "George Seuffert, Sr. Bandshell (in Forest Park), Queens",
+        "borough": "Queens",
+        "link": "https://forestparktrust.org/events/2024-summer-concert-series-4acsh"
+    },
+    {
+        "name": "Queens Day! (Concert with The Roots)",
+        "date": "2025-07-20",
+        "time": "10:00 AM – 8:00 PM (Festivities, concert time TBD)",
+        "address": "Flushing Meadows Corona Park, Queens",
+        "borough": "Queens",
+        "link": "https://allianceforfmcp.org/events/2025/7/20/queens-day"
+    },
+    {
+        "name": "2025 Waterfront Summer Concert Series (Astoria Park)",
+        "date": "2025-07-24",
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Great Lawn at Astoria Park, Queens",
+        "borough": "Queens",
+        "link": "https://www.nycgovparks.org/events/2025/07/17/2025-waterfront-summer-concert-series"
+    },
+    {
+        "name": "Summer Concert Series - Forest Park Trust (Standing Ovation - Musical Theater)",
+        "date": "2025-07-24",
+        "time": "7:30 PM",
+        "address": "George Seuffert, Sr. Bandshell (in Forest Park), Queens",
+        "borough": "Queens",
+        "link": "https://forestparktrust.org/events/2024-summer-concert-series-4acsh"
+    },
+    {
+        "name": "MoMA PS1 Warm Up",
+        "date": "July 2025 (specific Fridays TBD)",
+        "time": "TBD (likely evening)",
+        "address": "MoMA PS1, Long Island City, Queens",
+        "borough": "Queens",
+        "link": "https://www.timeout.com/newyork/events-calendar/july-events-calendar"
+    },
+    {
+        "name": "Carnegie Hall Citywide: Symphonic Brass Alliance",
+        "date": "2025-07-26",
+        "time": "TBD (likely evening)",
+        "address": "Historic Richmond Town, Staten Island", # Note: Source listed this under Queens/Staten Island
+        "borough": "Staten Island", # Marking as Staten Island based on address
+        "link": "https://secretnyc.co/free-summer-concerts-2025-full-list/"
+    },
+    {
+        "name": "2025 Waterfront Summer Concert Series (Astoria Park)",
+        "date": "2025-07-31",
+        "time": "7:00 PM – 9:00 PM",
+        "address": "Great Lawn at Astoria Park, Queens",
+        "borough": "Queens",
+        "link": "https://www.nycgovparks.org/events/2025/07/17/2025-waterfront-summer-concert-series"
+    },
+    {
+        "name": "Summer Concert Series - Forest Park Trust (Jay Bon Jovi - Bon Jovi Tribute)",
+        "date": "2025-07-31",
+        "time": "7:30 PM",
+        "address": "George Seuffert, Sr. Bandshell (in Forest Park), Queens",
+        "borough": "Queens",
+        "link": "https://forestparktrust.org/events/2024-summer-concert-series-4acsh"
+    },
+
+    # --- The Bronx Events ---
+    {
+        "name": "Sunset Wednesdays",
+        "date": "Starting 2025-07-09 (likely weekly or bi-weekly)",
+        "time": "TBD (likely evening)",
+        "address": "Wave Hill, The Bronx",
+        "borough": "The Bronx",
+        "link": "https://www.reddit.com/r/visitingnyc/comments/1kqmg4t/what_to_do_in_nyc_summer_2025_read_this_before/"
+    },
+
+    # --- Staten Island Events ---
+    {
+        "name": "Rosebank Street Tree Care (Volunteer Event)",
+        "date": "2025-07-26",
+        "time": "10:00 AM – 12:00 PM",
+        "address": "Von Briesen Park, 1271 Bay St (Shore Acres), Staten Island",
+        "borough": "Staten Island",
+        "link": "https://www.reddit.com/r/nyc/comments/1li14mo/things_to_do-in-nyc-july-2025/"
+    },
+    {
+        "name": "Rise Up NYC Concert Series (Staten Island Dates)",
+        "date": "2025-07-26 to 2025-07-27", # Series runs over these dates
+        "time": "TBD (check link closer to date)",
+        "address": "Venue details coming soon, Staten Island",
+        "borough": "Staten Island",
+        "link": "https://riseupnycconcerts.com/rise-up-nyc-2025-official-dates-announced-for-the-citys-biggest-free-concert-series/"
+    }
+]
+
+# Example of how you might access an event:
+# print(nyc_free_events_database[0]["name"])
+# print(nyc_free_events_database[0]["borough"])
+
+# Example of how you might iterate and print all event names:
+# for event in nyc_free_events_database:
+#     print(f"Event: {event['name']} ({event['borough']})")
